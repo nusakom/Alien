2ab1bd0f14f08173554e441e2479d16ded574dde
TENGY_Z <110544069+nusakom@users.noreply.github.com>
2025-02-03 10:52:02 +0000

    2/3

--- subsystems/vfs/src/init.rs
+++
@@ -1,9 +0,0 @@
-// vfs/src/init.rs
-use crate::dbfs2_fs::Dbfs2FS; // 引入你刚刚实现的 dbfs2 文件系统
-
-pub fn init_vfs() {
-    // 初始化 dbfs2 文件系统
-    let dbfs = Dbfs2FS::init("/path/to/db/database").unwrap();
-    // 进行挂载等操作
-    dbfs.mount("/mnt/dbfs").unwrap();
-}
