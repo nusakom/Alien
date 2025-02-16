bc148cc981c19352c4c5881ea1d1f21ad975d728
nusakom <2031263745@qq.com>
2025-02-16 14:19:52 +0800

    2/17

--- subsystems/vfs/src/initrd.rs
+++ subsystems/vfs/src/initrd.rs
@@ -51,20 +51,3 @@ fn parse_initrd_data(root: Arc<dyn VfsDentry>) -> AlienResult<()> {
     }
     Ok(())
 }
-let db = DB::open::<FileOpenOptions, _>(Arc::new(FakeMap), "my-database.db").unwrap();
-init_db(&db);
-dbfs2::init_dbfs(db); // 初始化全局的DBFS
-pub fn init_db(db: &DB, size: u64) {
-    let tx = db.tx(true).unwrap();
-    let bucket = tx.get_bucket("super_blk");
-    let bucket = if bucket.is_ok() {
-        return;
-    } else {
-        tx.create_bucket("super_blk").unwrap()
-    };
-    bucket.put("continue_number", 1usize.to_be_bytes()).unwrap();
-    bucket.put("magic", 1111u32.to_be_bytes()).unwrap();
-    bucket.put("blk_size", (SLICE_SIZE as u32).to_be_bytes()).unwrap();
-    bucket.put("disk_size", size.to_be_bytes()).unwrap(); //16MB
-    tx.commit().unwrap()
-}
